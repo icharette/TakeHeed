5ad770f9c8c4560af004bb9ea93709e3de4e0187
icharette <isabelle.b.charette@gmail.com>
2020-01-29 16:13:36 -0500

    added test cases

--- motors_IMU_neopixels/src/motors_IMU_neopixels.ino
+++ motors_IMU_neopixels/src/motors_IMU_neopixels.ino
@@ -95,7 +95,7 @@ float gainThreshold, lossThreshold;
  float fluxY = 0;
  float fluxZ = 0;
 int speedLimit = 20;
-boolean calibrated = false;
+boolean calibrated = true;
 
 #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
@@ -199,7 +199,7 @@ void calibrateSensor(){
  
 Stepper stepper(STEPS, A1, A2, A3, A4);
 
-bool onlyMotor = true;
+bool onlyMotor = false;
 
 void setup() {
   pinMode(D7, OUTPUT);
@@ -242,63 +242,67 @@ void setup() {
 
 //-----------------------//-----------------------//-----------------------//-----------------------LOOPING
 void loop() {
-//STEPPER motors--------------------------------------------------------------------------------------------------------------------------------------
-//PIN SETUP: Stepper stepper(STEPS, A1, A2, A3, A4);
-/*
-stepper.setSpeed(50);
-digitalWrite(D7, HIGH);
-Serial.println("LED ON");
-stepper.step(-STEPS);
-stepper.setSpeed(44); 
-digitalWrite(D7, LOW);  
-Serial.println("LED OFF");            ;
-stepper.step(STEPS);
-*/
 
-//LED STRIP--------------------------------------------------------------------------------------------------------------------------------------
-//examples from neopixels library
-// PIN SETUP: PIXEL_PIN D2
-// PIXEL_COUNT 20; can change at line 61
-/*
-strip.Color(255,255,255);
-Serial.println("Red"); 
-colorWipe(strip.Color(255, 0, 0), 50); // Red
-Serial.println("Green"); 
-colorWipe(strip.Color(0, 255, 0), 50); // Green
-Serial.println("Blue"); 
-colorWipe(strip.Color(0, 0, 255), 50); // Blue
-*/
-
-//IMU--------------------------------------------------------------------------------------------------------------------------------------
-// ***-->IMPORTANT: set onlyMotor boolean to false when uncommenting following functions
-// *** -->set back to TRUE when this is commented out. not needed.
-// **other wise you get a wierd error, the board flashes red and even if you flash the right
-// ***code afterwards, it still flashes red and won't work sometimes. If this happens, audit the environment, clean it and reset the particle into DFU mode (flashing yellow, then try flashing again)
-// *DFU mode: hold told *reset* and *setup*, only release *reset* and keep holding down *setup* until the led cycles to yellow light
-// *if D7 LED on particle board lights up; problem with IMU sensor connection
-// PIN setup:
-
-// 	LSM9DS1 --------- Photon
-// 	 SCL -------------- D1 (SCL)
-// 	 SDA -------------- D0 (SDA)
-// 	 VDD ------------- 3.3V
-// 	 GND ------------- GND
-
-/*
-getMouvement();
-printMvmt();
-*/
-
-//coral activity-------------------------------------------------------------------------------------------------------------------------------------- 
-// ***-->IMPORTANT: set onlyMotor boolean to false when uncommenting following functions
-/*
-updateTimer.Update();
-healthyWave(10,10,1);
-*/
+testCase(0);
+testCase(1);
+//-->IMPORTANT: only set boolean onlyMotor to true when using next testcase
+//testCase(2);
 }
 
 //-----------------------//-----------------------//-----------------------//-----------------------LOOPING
 
+void testCase(int component){
+  switch(component){
+    case 0:
+    //STEPPER motors--------------------------------------------------------------------------------------------------------------------------------------
+    //PIN SETUP: Stepper stepper(STEPS, A1, A2, A3, A4);
+    stepper.setSpeed(50);
+    digitalWrite(D7, HIGH);
+    Serial.println("LED ON");
+    stepper.step(-STEPS);
+    stepper.setSpeed(44); 
+    digitalWrite(D7, LOW);  
+    Serial.println("LED OFF");            ;
+    stepper.step(STEPS);
+    break;
+
+    case 1:
+    //LED STRIP--------------------------------------------------------------------------------------------------------------------------------------
+    //examples from neopixels library
+    // PIN SETUP: PIXEL_PIN D2
+    // PIXEL_COUNT 20; can change at line 61
+    strip.Color(255,255,255);
+    Serial.println("Red"); 
+    colorWipe(strip.Color(255, 0, 0), 50); // Red
+    Serial.println("Green"); 
+    colorWipe(strip.Color(0, 255, 0), 50); // Green
+    Serial.println("Blue"); 
+    colorWipe(strip.Color(0, 0, 255), 50); // Blue
+    break;
+
+    case 2:
+    //IMU--------------------------------------------------------------------------------------------------------------------------------------
+    // ***-->IMPORTANT: set onlyMotor boolean to false when uncommenting following functions
+    // *** -->set back to TRUE when this is commented out. not needed.
+    // **other wise you get a wierd error, the board flashes red and even if you flash the right
+    // ***code afterwards, it still flashes red and won't work sometimes. If this happens, audit the environment, clean it and reset the particle into DFU mode (flashing yellow, then try flashing again)
+    // *DFU mode: hold told *reset* and *setup*, only release *reset* and keep holding down *setup* until the led cycles to yellow light
+    // *if D7 LED on particle board lights up; problem with IMU sensor connection
+    // PIN setup:
+
+    // 	LSM9DS1 --------- Photon
+    // 	 SCL -------------- D1 (SCL)
+    // 	 SDA -------------- D0 (SDA)
+    // 	 VDD ------------- 3.3V
+    // 	 GND ------------- GND
+    getMouvement();
+    printMvmt();
+    break;
+
+    default:
+    break;
+  }
+}
 
 
 //-----------------------//-----------------------//-----------------------//-----------------------COMMUNICATION
